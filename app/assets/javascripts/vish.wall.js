xwiki-labs/vish

commit 9b449963a6b124de8161361a9bc876275110c4d7

AJAX-ly asynchronous modals. WIP There is a bug: you need to click twice to open a modal!

app/assets/javascripts/vish.wall.js
@@ -110,7 +110,7 @@ Vish.Wall = (function(V, $, undefined){
     if (modal.length) {
       return modal;
     } else {
-      return $('<div class="modal hide" id="' + klass + '-modal-' + id + '"><div class="modal-header"><h3 class="text-center">' + title + '</h3></div><div id="'+klass+'-modal-body-'+id+'" class="modal-body text-center">'+ modalPayload(klass, id) +'</div><div class="modal-footer"><div class="pull-left">' + modalLikeBtn(signed_in, activity_id, is_fav) + '</div><div class="pull-right"><a href="#" class="btn btn-danger ' + klass + '-modal-close-' + id + '" data-dismiss="modal">'+ I18n.t('close') +'</a><a href="/' + klass + 's/' + id + '" class="btn btn-success">' + I18n.t('details.msg') + '</a></div></div></div>').appendTo($('body'));
+      return $('<div class="modal hide" data-show="false" id="' + klass + '-modal-' + id + '"><div class="modal-header"><h3 class="text-center">' + title + '</h3></div><div id="'+klass+'-modal-body-'+id+'" class="modal-body text-center">'+ modalPayload(klass, id) +'</div><div class="modal-footer"><div class="pull-left">' + modalLikeBtn(signed_in, activity_id, is_fav) + '</div><div class="pull-right"><a href="#" class="btn btn-danger ' + klass + '-modal-close-' + id + '" data-dismiss="modal">'+ I18n.t('close') +'</a><a href="/' + klass + 's/' + id + '" class="btn btn-success">' + I18n.t('details.msg') + '</a></div></div></div>').appendTo($('body'));
     }
   }
 
@@ -119,7 +119,7 @@ Vish.Wall = (function(V, $, undefined){
     if (modal.length) {
       return modal;
     } else {
-      return $('<div class="modal hide" id="user-modal-' + id + '"><div class="modal-header"><h3 class="text-center">' + name + '</h3></div><div id="user-modal-body-'+id+'" class="modal-body text-center"><img alt="Loading" class="loading" src="/assets/loading.gif" /></div><div class="modal-footer"><div class="pull-left">' + modalFollowBtn(signed_in, contact_link) + '</div><div class="pull-right"><a href="#" class="btn btn-danger user-modal-close-' + id + '" data-dismiss="modal">'+ I18n.t('close') +'</a><a href="/users/' + id + '" class="btn btn-success">' + I18n.t('details.msg') + '</a></div></div></div>').appendTo($('body'));
+      return $('<div class="modal hide" data-show="false" id="user-modal-' + id + '"><div class="modal-header"><h3 class="text-center">' + name + '</h3></div><div id="user-modal-body-'+id+'" class="modal-body text-center"><img alt="Loading" class="loading" src="/assets/loading.gif" /></div><div class="modal-footer"><div class="pull-left">' + modalFollowBtn(signed_in, contact_link) + '</div><div class="pull-right"><a href="#" class="btn btn-danger user-modal-close-' + id + '" data-dismiss="modal">'+ I18n.t('close') +'</a><a href="/users/' + id + '" class="btn btn-success">' + I18n.t('details.msg') + '</a></div></div></div>').appendTo($('body'));
     }
   }
 

app/helpers/activities_helper.rb
@@ -2,9 +2,20 @@ module ActivitiesHelper
   # Javascript line to fetch or create the modal dialog
   def modal_for(object)
     if object.acts_as_actor?
-      "Vish.Wall.getUserModal('#{object.slug}', #{ user_signed_in? ? "true" : "false" }, '#{raw j truncate_name(object.name, :length => 75)}', '#{ raw j contact_to(object)}'); $.ajax('/users/#{ object.actor.slug }/modal');"
+      "m = Vish.Wall.getUserModal('#{object.slug}', #{ user_signed_in? ? "true" : "false" }, '#{raw j truncate_name(object.name, :length => 75)}', '#{ raw j contact_to(object)}');
+       m.on('shown', function(){
+         $.ajax('/users/#{ object.actor.slug }/modal');
+       });
+       m.on('hidden', function(){
+       });"
     else
-      "Vish.Wall.getModal('#{object.class.to_s.downcase}', '#{object.id.to_s}', #{ user_signed_in? ? "true" : "false" }, #{object.activities.first.id.to_s}, #{ user_signed_in? and object.activities.first.liked_by?(current_subject) ? "true" : "false" }, '#{ raw j truncate_name(object.title , :length => 75) }');"
+      "m = Vish.Wall.getModal('#{object.class.to_s.downcase}', '#{object.id.to_s}', #{ user_signed_in? ? "true" : "false" }, #{object.activities.first.id.to_s}, #{ user_signed_in? and object.activities.first.liked_by?(current_subject) ? "true" : "false" }, '#{ raw j truncate_name(object.title , :length => 75) }');
+       m.on('shown', function(){
+         $.ajax('/#{ object.class.to_s.pluralize.underscore }/#{ object.id.to_s }/modal');
+       });
+       m.on('hidden', function(){
+         $('##{ object.class.to_s.downcase }-modal-#{ object.id.to_s } .modal-body').html ('#{ raw j image_tag("loading.gif", :class => :loading) }');
+       });"
     end
   end
 

app/views/audios/_audio_processed.html.erb
@@ -17,22 +17,6 @@
 </ul>
 
 <%= javascript_tag do %>
-  $(".audio-modal-button-<%= audio.id.to_s%>").on("click", function(){
-    <%= modal_for(audio) %>
-    $("#jpId<%=audio.id%>").jPlayer( {
-      ready: function () {
-        $(this).jPlayer("setMedia", {
-          webma: "<%= audio_url(audio, :format => 'webma') %>" // Defines the webma url
-        });
-      },
-      solution:"webma",
-      preload: "none",
-      swfPath: "assets",
-      cssSelectorAncestor: "#jp_interface_<%=audio.id%>"
-    });
-    $("#audio-modal-<%= audio.id.to_s%>").on("hidden", function(){
-      $("#jpId<%=audio.id%>").jPlayer('stop');
-    });
-  });
+  <%= modal_for(audio) %>
 <% end %>
 

app/views/comments/_comment_global_search.html.erb
@@ -18,7 +18,5 @@
 </div>
 
 <%= javascript_tag do %>
-  $(".user-modal-button-<%= author.slug %>").on("click", function(){
-    <%= modal_for(author.subject) %>
-  });
+  <%= modal_for(author.subject) %>
 <% end %>

app/views/common_documents/_document_info.html.erb
@@ -166,9 +166,7 @@
   <div class="space_center"></div>
 
   <%= javascript_tag do %>
-    $(".user-modal-button-<%= document.author.slug %>").on("click", function(){
-      <%= modal_for(document.author.subject) %>
-    });
+    <%= modal_for(document.author.subject) %>
   <% end %>
 
 

app/views/common_documents/_home.html.erb
@@ -19,57 +19,8 @@
   </ul>
 
   <%= javascript_tag do %>
-    $(".<%= document.class.to_s.downcase %>-modal-button-<%= document.id.to_s%>").on("click", function(){
-      <%= modal_for(document) %>
-      <% if document.is_a? Swf %>
-        $("#swf-modal-<%= document.id.to_s%> .modal-body").css('padding', '0');
-        $("#swf-modal-<%= document.id.to_s%> .modal-body").html("<iframe src='<%= document.file %>' width='545px' height='385px'>...</iframe>");
-      <% elsif document.is_a? Officedoc %>
-        $("#officedoc-modal-<%= document.id.to_s%> .modal-body").css('padding', '0');
-        $("#officedoc-modal-<%= document.id.to_s%> .modal-body").html("<iframe src='http://docs.google.com/viewer?url=<%= CGI::escapeHTML( 'http://' + request.env['HTTP_HOST'] + download_document_path(document)) %>&embedded=true' width='545px' height='385px' style='border: none;'></iframe>");
-      <% elsif document.is_a? Picture %>
-        $("#picture-modal-<%= document.id.to_s%> .modal-body").html("<%= raw j (thumb_for(document, 500)) %>");
-      <% elsif document.is_a? Audio %>
-        $("#jpId<%=document.id%>").jPlayer( {
-          ready: function () {
-            $(this).jPlayer("setMedia", {
-              webma: "<%= audio_url(document, :format => 'webma') %>" // Defines the webma url
-            });
-          },
-          solution:"webma",
-          preload: "none",
-          swfPath: "assets",
-          cssSelectorAncestor: "#jp_interface_<%=document.id%>"
-        });
-      <% elsif document.is_a? Video %>
-        $("#jpId<%=document.id%>").jPlayer( {
-          ready: function () {
-            $(this).jPlayer("setMedia", {
-              webmv: "<%= video_url(document, :format => 'webm' ) %>", // Defines the webm url
-              flv:   "<%= video_url(document, :format => 'flv') %>",
-              mp4:   "<%= video_url(document, :format => 'mp4') %>"
-            });
-          },
-          solution:"flash, html",
-          preload: "none",
-          supplied: "webmv, flv, mp4",
-          swfPath: "assets",
-          cssSelectorAncestor: "#jp_interface_<%=document.id%>"
-        });
-      <% end %>
-      $("#<%= document.class.to_s.downcase %>-modal-<%= document.id.to_s%>").on("hidden", function(){
-        <% if document.is_a? Audio or document.is_a? Video %>
-          $("#jpId<%=document.id%>").jPlayer('stop');
-        <% else %>
-          $("#<%= document.class.to_s.downcase %>-modal-<%= document.id.to_s%> .modal-body").html ("<%= raw j image_tag('loading.gif', :class => :loading) %>");
-        <% end %>
-      });
-    });
+    <%= modal_for(document) %>
+    <%= modal_for(document.author.subject) %>
   <% end %>
 <% end %>
 
-<%= javascript_tag do %>
-  $(".user-modal-button-<%= document.author.slug %>").on("click", function(){
-    <%= modal_for(document.author.subject) %>
-  });
-<% end %>

app/views/contacts/_contact.html.erb
@@ -20,9 +20,7 @@
   <div class="space_center">
   </div>
   <%= javascript_tag do %>
-    $(".user-modal-button-<%= contact.receiver.slug %>").on("click", function(){
-      <%= modal_for(contact.receiver_subject) %>
-    });
+    <%= modal_for(contact.receiver_subject) %>
   <% end %>
 <% end %>
 

commit 1582e0bbd255fd1680744174334aff8c413c220c

Don't hide modals twice, create them before using them. This fixes the issue introduced in 9b449963a6b124de8161361a9bc876275110c4d7

app/assets/javascripts/vish.wall.js
@@ -110,7 +110,7 @@ Vish.Wall = (function(V, $, undefined){
     if (modal.length) {
       return modal;
     } else {
-      return $('<div class="modal hide" data-show="false" id="' + klass + '-modal-' + id + '"><div class="modal-header"><h3 class="text-center">' + title + '</h3></div><div id="'+klass+'-modal-body-'+id+'" class="modal-body text-center">'+ modalPayload(klass, id) +'</div><div class="modal-footer"><div class="pull-left">' + modalLikeBtn(signed_in, activity_id, is_fav) + '</div><div class="pull-right"><a href="#" class="btn btn-danger ' + klass + '-modal-close-' + id + '" data-dismiss="modal">'+ I18n.t('close') +'</a><a href="/' + klass + 's/' + id + '" class="btn btn-success">' + I18n.t('details.msg') + '</a></div></div></div>').appendTo($('body'));
+      return $('<div class="modal hide" id="' + klass + '-modal-' + id + '"><div class="modal-header"><h3 class="text-center">' + title + '</h3></div><div id="'+klass+'-modal-body-'+id+'" class="modal-body text-center">'+ modalPayload(klass, id) +'</div><div class="modal-footer"><div class="pull-left">' + modalLikeBtn(signed_in, activity_id, is_fav) + '</div><div class="pull-right"><a href="#" class="btn btn-danger ' + klass + '-modal-close-' + id + '" data-dismiss="modal">'+ I18n.t('close') +'</a><a href="/' + klass + 's/' + id + '" class="btn btn-success">' + I18n.t('details.msg') + '</a></div></div></div>').appendTo($('body'));
     }
   }
 
@@ -119,7 +119,7 @@ Vish.Wall = (function(V, $, undefined){
     if (modal.length) {
       return modal;
     } else {
-      return $('<div class="modal hide" data-show="false" id="user-modal-' + id + '"><div class="modal-header"><h3 class="text-center">' + name + '</h3></div><div id="user-modal-body-'+id+'" class="modal-body text-center"><img alt="Loading" class="loading" src="/assets/loading.gif" /></div><div class="modal-footer"><div class="pull-left">' + modalFollowBtn(signed_in, contact_link) + '</div><div class="pull-right"><a href="#" class="btn btn-danger user-modal-close-' + id + '" data-dismiss="modal">'+ I18n.t('close') +'</a><a href="/users/' + id + '" class="btn btn-success">' + I18n.t('details.msg') + '</a></div></div></div>').appendTo($('body'));
+      return $('<div class="modal hide" id="user-modal-' + id + '"><div class="modal-header"><h3 class="text-center">' + name + '</h3></div><div id="user-modal-body-'+id+'" class="modal-body text-center"><img alt="Loading" class="loading" src="/assets/loading.gif" /></div><div class="modal-footer"><div class="pull-left">' + modalFollowBtn(signed_in, contact_link) + '</div><div class="pull-right"><a href="#" class="btn btn-danger user-modal-close-' + id + '" data-dismiss="modal">'+ I18n.t('close') +'</a><a href="/users/' + id + '" class="btn btn-success">' + I18n.t('details.msg') + '</a></div></div></div>').appendTo($('body'));
     }
   }
 

app/helpers/activities_helper.rb
@@ -3,14 +3,14 @@ module ActivitiesHelper
   def modal_for(object)
     if object.acts_as_actor?
       "m = Vish.Wall.getUserModal('#{object.slug}', #{ user_signed_in? ? "true" : "false" }, '#{raw j truncate_name(object.name, :length => 75)}', '#{ raw j contact_to(object)}');
-       m.on('shown', function(){
+       m.on('show', function(){
          $.ajax('/users/#{ object.actor.slug }/modal');
        });
        m.on('hidden', function(){
        });"
     else
       "m = Vish.Wall.getModal('#{object.class.to_s.downcase}', '#{object.id.to_s}', #{ user_signed_in? ? "true" : "false" }, #{object.activities.first.id.to_s}, #{ user_signed_in? and object.activities.first.liked_by?(current_subject) ? "true" : "false" }, '#{ raw j truncate_name(object.title , :length => 75) }');
-       m.on('shown', function(){
+       m.on('show', function(){
          $.ajax('/#{ object.class.to_s.pluralize.underscore }/#{ object.id.to_s }/modal');
        });
        m.on('hidden', function(){

app/views/audios/_audio_processed.html.erb
@@ -1,4 +1,7 @@
 <ul class="thumbnails">
+  <%= javascript_tag do %>
+    <%= modal_for(audio) %>
+  <% end %>
   <li class="span3">
     <a data-toggle="modal" href="#audio-modal-<%= audio.id.to_s%>" class="audio-modal-button-<%= audio.id.to_s%>"><i class='icon75-audio'></i></a>
   </li>
@@ -15,8 +18,4 @@
     </div>
   </li>
 </ul>
-
-<%= javascript_tag do %>
-  <%= modal_for(audio) %>
-<% end %>
 

app/views/comments/_comment_global_search.html.erb
@@ -1,5 +1,8 @@
 <% author = comment.author_subject %>
 <div class="model_with_details">
+  <%= javascript_tag do %>
+    <%= modal_for(author.subject) %>
+  <% end %>
   <div class="logo">
     <a data-toggle="modal" href="#user-modal-<%= author.slug %>" class="user-modal-button-<%= author.slug %>"><%= image_tag(author.logo.url, :size => "50x50", :alt => author.name) %></a>
   </div>
@@ -17,6 +20,3 @@
   </div>
 </div>
 
-<%= javascript_tag do %>
-  <%= modal_for(author.subject) %>
-<% end %>

app/views/common_documents/_document_info.html.erb
@@ -1,4 +1,8 @@
 <div class="all-description">
+  <%= javascript_tag do %>
+    <%= modal_for(document.author.subject) %>
+  <% end %>
+
   <div id=" notebook-menu" class="tabbable span3 inline-block">
     <ul class="nav nav-tabs">
       <li class="active">
@@ -164,11 +168,6 @@
   </div>
 
   <div class="space_center"></div>
-
-  <%= javascript_tag do %>
-    <%= modal_for(document.author.subject) %>
-  <% end %>
-
 
 </div>
 

app/views/common_documents/_home.html.erb
@@ -1,4 +1,9 @@
 <%= div_for document, :class => 'span3' do %>
+  <%= javascript_tag do %>
+    <%= modal_for(document) %>
+    <%= modal_for(document.author.subject) %>
+  <% end %>
+
   <ul class="thumbnails">
     <li class="span">
       <div id="margin-resources">
@@ -17,10 +22,5 @@
       </div>
     </div>
   </ul>
-
-  <%= javascript_tag do %>
-    <%= modal_for(document) %>
-    <%= modal_for(document.author.subject) %>
-  <% end %>
 <% end %>
 

app/views/contacts/_contact.html.erb
@@ -1,4 +1,7 @@
 <%= div_for contact, :class => 'row' do %>
+  <%= javascript_tag do %>
+    <%= modal_for(contact.receiver_subject) %>
+  <% end %>
   <ul class="thumbnails span3">
     <li class="span1">
       <a data-toggle="modal" href="#user-modal-<%= contact.receiver.slug %>" class="user-modal-button-<%= contact.receiver.slug %> container"><%= image_tag(contact.receiver.logo.url(:profile)) %></a>
@@ -19,9 +22,6 @@
   </ul>
   <div class="space_center">
   </div>
-  <%= javascript_tag do %>
-    <%= modal_for(contact.receiver_subject) %>
-  <% end %>
 <% end %>
 
 

app/views/documents/_document_focus_search.html.erb
@@ -1,4 +1,7 @@
 <div class="model_with_details span3 box-result">
+  <%= javascript_tag do %>
+    <%= modal_for(document.author.subject) %>
+  <% end %>
   <ul class="thumbnails">
     <li class="span1 margin-clear">
       <%= link_to  "", document, :class => icon75_class_for(document) , :alt => document.title %>
@@ -17,6 +20,3 @@
   </ul>
 </div>
 
-<%= javascript_tag do %>
-  <%= modal_for(document.author.subject) %>
-<% end %>

app/views/documents/_document_global_search.html.erb
@@ -1,4 +1,7 @@
 <div class="model_with_details span3 box-result">
+  <%= javascript_tag do %>
+    <%= modal_for(document.author.subject) %>
+  <% end %>
   <ul class="thumbnails">
     <li class="span1 margin-clear">
       <%= link_to  "", document, :class => icon75_class_for(document) , :alt => document.title %>
@@ -17,6 +20,3 @@
   </ul>
 </div>
 
-<%= javascript_tag do %>
-  <%= modal_for(document.author.subject) %>
-<% end %>

app/views/embeds/_embed.html.erb
@@ -1,4 +1,8 @@
 <%= div_for embed, :class => 'row-fluid' do %>
+  <%= javascript_tag do %>
+    <%= modal_for(embed) %>
+  <% end %>
+
   <ul class="thumbnails">
     <li class="span3">
       <a data-toggle="modal" href="#embed-modal-<%= embed.id.to_s%>" class="embed-modal-button-<%= embed.id.to_s%>"><i class="icon_repository75-75_embed"></i></a>
@@ -14,11 +18,6 @@
       <p><%= link_to t('details.msg'), embed, :class => "attachment_text_link" %></p>
     </li>
   </ul>
-
-  <%= javascript_tag do %>
-    <%= modal_for(embed) %>
-  <% end %>
-
 <% end %>
 
 <div class="clearfloat"></div>

app/views/embeds/_embed_focus_search.html.erb
@@ -1,4 +1,8 @@
 <div class="model_with_details span3 box-result">
+  <%= javascript_tag do %>
+    <%= modal_for(embed.author.subject) %>
+  <% end %>
+
   <ul class="thumbnails">
     <li class="span1 margin-clear">
       <%= link_to  "", embed, :class => icon75_class_for(embed) , :alt => embed.title %>
@@ -16,8 +20,4 @@
     </li>
   </ul>
 </div>
-
-<%= javascript_tag do %>
-  <%= modal_for(embed.author.subject) %>
-<% end %>
 

app/views/embeds/_embed_global_search.html.erb
@@ -1,4 +1,8 @@
 <div class="model_with_details span3 box-result">
+  <%= javascript_tag do %>
+    <%= modal_for(embed.author.subject) %>
+  <% end %>
+
   <ul class="thumbnails">
     <li class="span1 margin-clear">
       <%= link_to  "", embed, :class => icon75_class_for(embed) , :alt => embed.title %>
@@ -17,6 +21,3 @@
   </ul>
 </div>
 
-<%= javascript_tag do %>
-  <%= modal_for(embed.author.subject) %>
-<% end %>

app/views/embeds/_home.html.erb
@@ -1,4 +1,8 @@
 <%= div_for embed, :class => 'span3' do %>
+  <%= javascript_tag do %>
+    <%= modal_for(embed) %>
+    <%= modal_for(embed.author.subject) %>
+  <% end %>
   <ul class="thumbnails">
     <li class="span">
       <div id="margin-resources">
@@ -17,9 +21,4 @@
         </div>
     </div>
   </ul>
-
-  <%= javascript_tag do %>
-    <%= modal_for(embed) %>
-    <%= modal_for(embed.author.subject) %>
-  <% end %>
 <% end %>

app/views/excursions/_excursion_focus_search.html.erb
@@ -1,4 +1,8 @@
 <div class="model_with_details span3 box-result">
+  <%= javascript_tag do %>
+    <%= modal_for(excursion.author.subject) %>
+  <% end %>
+
   <ul class="thumbnails">
     <li class="span1 margin-clear">
       <%= link_to  excursion_thumb_for(excursion, 130), excursion, :class => "container" %>
@@ -17,8 +21,4 @@
     </li>
   </ul>
 </div>
-
-<%= javascript_tag do %>
-  <%= modal_for(excursion.author.subject) %>
-<% end %>
 

app/views/excursions/_excursion_global_search.html.erb
@@ -1,4 +1,7 @@
 <div class="model_with_details span3 box-result">
+  <%= javascript_tag do %>
+    <%= modal_for(excursion.author.subject) %>
+  <% end %>
   <ul class="thumbnails">
     <li class="span1 margin-clear">
       <%= link_to  excursion_thumb_for(excursion, 130), excursion, :class => "container" %>
@@ -18,6 +21,3 @@
   </ul>
 </div>
 
-<%= javascript_tag do %>
-  <%= modal_for(excursion.author.subject) %>
-<% end %>

app/views/excursions/_excursion_info.html.erb
@@ -1,3 +1,7 @@
+<%= javascript_tag do %>
+  <%= modal_for(@excursion.author.subject) %>
+<% end %>
+
 <div class="space_center"></div>
 <div class="notebook">
   <div class="excursion-notebook">
@@ -116,8 +120,4 @@
   <div class="notebook-pie"></div>
 </div>
 <div class="space_center"></div>
-
-<%= javascript_tag do %>
-  <%= modal_for(@excursion.author.subject) %>
-<% end %>
 

app/views/excursions/_home.html.erb
@@ -1,4 +1,7 @@
 <% unless excursion.blank? %>
+  <%= javascript_tag do %>
+    <%= modal_for(excursion.author.subject) %>
+  <% end %>
   <%= div_for excursion, :class => 'span3' do %>
     <ul class="thumbnails ">
         <li class="span1">
@@ -22,6 +25,3 @@
   <% end %>
 <% end %>
 
-<%= javascript_tag do %>
-  <%= modal_for(excursion.author.subject) %>
-<% end %>

app/views/home/index.html.erb
@@ -185,6 +185,9 @@
 
           <div class="follow-home-box ">
           <% current_subject.following_actor_objects.includes(:actor).all.shuffle.first(6).each do |object|  %>
+            <%= javascript_tag do %>
+              <%= modal_for(object.actor.subject) %>
+            <% end %>
 
             <%= div_for object.actor, :class => '' do %>
               <ul class="thumbnails span">
@@ -193,9 +196,6 @@
                 </li>
               </ul>
             <% end %>
-          <%= javascript_tag do %>
-            <%= modal_for(object.actor.subject) %>
-          <% end %>
           <% end %>
           </div>
           <div class="more"><%= link_to(t('view_all'), followings_path) %></div>
@@ -205,6 +205,9 @@
           <%= render :partial => "followers/filter", :locals => {:show_add => true} %>
           <div class="follow-home-box ">
           <% current_subject.followers.all.shuffle.first(6).each do |object|  %>
+            <%= javascript_tag do %>
+              <%= modal_for(object.actor.subject) %>
+            <% end %>
 
             <%= div_for object.actor, :class => '' do %>
               <ul class="thumbnails span">
@@ -212,9 +215,6 @@
                   <a data-toggle="modal" href="#user-modal-<%= object.actor.slug %>" class="user-modal-button-<%= object.actor.slug %> container follower-tooltip" title='<%= truncate(object.actor.name, :length => 20) %>' rel='tooltip'><%= image_tag(object.actor.logo.url(:contact)) %></a>
                 </li>
               </ul>
-            <% end %>
-            <%= javascript_tag do %>
-              <%= modal_for(object.actor.subject) %>
             <% end %>
           <% end %>
           <div class="more"><%= link_to(t('view_all'), followers_path) %></div>

app/views/links/_home.html.erb
@@ -1,6 +1,11 @@
 <%= div_for link, :class => 'span3', :style => 'padding: 0; margin-top: 0;' do %>
   <% link_title = link.title %>
   <% link_title = link.url if link_title.blank? %>
+  <%= javascript_tag do %>
+    <%= modal_for(link) %>
+    <%= modal_for(link.author.subject) %>
+  <% end %>
+
   <ul class="thumbnails">
     <li class="span">
       <div id="margin-resources">
@@ -22,10 +27,5 @@
         </div>
     </div>
   </ul>
-
-  <%= javascript_tag do %>
-    <%= modal_for(link) %>
-    <%= modal_for(link.author.subject) %>
-  <% end %>
 
 <% end %>

app/views/links/_link.html.erb
@@ -1,4 +1,8 @@
 <%= div_for link, :class => 'row-fluid' do %>
+  <%= javascript_tag do %>
+    <%= modal_for(link) %>
+  <% end %>
+
   <ul class="thumbnails">
     <li class="span3">
       <% if link.callback_url %><a data-toggle="modal" href="#link-modal-<%= link.id.to_s%>" class="link-modal-button-<%= link.id.to_s%>"><% else %><a href="<%= link.url %>" target="_blank" class="container"><% end %><% if link.image %><%= image_tag(link.image, :size =>"75x75") %><% else %><i class='icon_repository75-75_link'></i> <% end %></a>
@@ -15,10 +19,6 @@
 
     </li>
   </ul>
-
-  <%= javascript_tag do %>
-    <%= modal_for(link) %>
-  <% end %>
 
 <% end %>
   <%= javascript_tag do %>

app/views/links/_link_focus_search.html.erb
@@ -1,4 +1,7 @@
 <div class="model_with_details span3 box-result">
+  <%= javascript_tag do %>
+    <%= modal_for(link.author.subject) %>
+  <% end %>
   <ul class="thumbnails">
     <li class="span1 margin-clear">
       <%= link_to  "", link, :class => icon75_class_for(link) , :alt => link.title %>
@@ -15,8 +18,4 @@
     </li>
   </ul>
 </div>
-
-<%= javascript_tag do %>
-  <%= modal_for(link.author.subject) %>
-<% end %>
 

app/views/links/_link_global_search.html.erb
@@ -1,4 +1,7 @@
 <div class="model_with_details span3 box-result">
+  <%= javascript_tag do %>
+    <%= modal_for(link.author.subject) %>
+  <% end %>
   <ul class="thumbnails">
     <li class="span1 margin-clear">
       <%= link_to  "", link, :class => icon75_class_for(link) , :alt => link.title %>
@@ -16,6 +19,3 @@
   </ul>
 </div>
 
-<%= javascript_tag do %>
-  <%= modal_for(link.author.subject) %>
-<% end %>

app/views/officedocs/_officedoc.html.erb
@@ -1,4 +1,7 @@
 <%= div_for officedoc, :class => 'row-fluid' do %>
+  <%= javascript_tag do %>
+    <%= modal_for(officedoc) %>
+  <% end %>
   <ul class="thumbnails">
     <li class="span3">
       <a data-toggle="modal" href="#officedoc-modal-<%= officedoc.id.to_s%>" class="officedoc-modal-button-<%= officedoc.id.to_s%>"><i class='icon75-pdf'></i></a>
@@ -13,10 +16,6 @@
       <p><%= link_to t('details.msg'), officedoc, :class => "attachment_text_link" %></p>
     </li>
   </ul>
-
-  <%= javascript_tag do %>
-    <%= modal_for(officedoc) %>
-  <% end %>
 <% end %>
 
 <div class="clearfloat"></div>

app/views/pictures/_picture.html.erb
@@ -1,4 +1,7 @@
 <%= div_for picture, :class => 'row-fluid' do %>
+  <%= javascript_tag do %>
+    <%= modal_for(picture) %>
+  <% end %>
   <ul class="thumbnails">
     <li class="span3">
       <a data-toggle="modal" href="#picture-modal-<%= picture.id.to_s%>" class="picture-modal-button-<%= picture.id.to_s%>"><%= thumb_for(picture, 75) %></a>
@@ -16,11 +19,6 @@
       <p><%= link_to t('details.msg'), picture, :class => "attachment_text_link" %></p>
     </li>
   </ul>
-
-  <%= javascript_tag do %>
-    <%= modal_for(picture) %>
-  <% end %>
-
 <% end %>
 
 <div class="clearfloat"></div>

app/views/posts/_post_global_search.html.erb
@@ -1,5 +1,8 @@
 <% author = post.author_subject %>
 <div class="model_with_details span3  box-result">
+  <%= javascript_tag do %>
+    <%= modal_for(author.subject) %>
+  <% end %>
   <ul class="thumbnails">
     <li class="span1 margin-clear ">
       <a data-toggle="modal" href="#user-modal-<%= author.slug %>" class="user-modal-button-<%= author.slug %>"><%= image_tag(author.logo.url, :alt => author.name) %></a>
@@ -22,6 +25,3 @@
   </ul>
 </div>
 
-<%= javascript_tag do %>
-  <%= modal_for(author.subject) %>
-<% end %>

app/views/posts/_post_with_details.html.erb
@@ -1,5 +1,8 @@
 <% author = post.author_subject %>
 <div class="row-fluid">
+  <%= javascript_tag do %>
+    <%= modal_for(author.subject) %>
+  <% end %>
   <div class="span2 pagination-centered link_white">
     <a data-toggle="modal" href="#user-modal-<%= author.slug %>" class="user-modal-button-<%= author.slug %>" alt='<%= author.name %>'><%= image_tag(author.logo.url(:contact)) %></a>
   </div>
@@ -16,6 +19,3 @@
   </div>
 </div>
 
-<%= javascript_tag do %>
-  <%= modal_for(author.subject) %>
-<% end %>

app/views/swfs/_swf.html.erb
@@ -1,4 +1,8 @@
 <%= div_for swf, :class => 'row-fluid' do %>
+  <%= javascript_tag do %>
+    <%= modal_for(swf) %>
+  <% end %>
+
 	<ul class="thumbnails">
     <li class="span3">
       <a data-toggle="modal" href="#swf-modal-<%= swf.id.to_s%>" class="swf-modal-button-<%= swf.id.to_s%>"><i class='icon75-swf'></i></a>
@@ -14,10 +18,6 @@
       <p><%= link_to t('details.msg'), swf, :class => "attachment_text_link" %></p>
     </li>
 	</ul>
-
-  <%= javascript_tag do %>
-    <%= modal_for(swf) %>
-  <% end %>
 
 <% end %>
 

app/views/toolbar/_contacts.html.erb
@@ -4,6 +4,9 @@
 <% subject.followers.all.shuffle.first(4).each do |object|  %>
 
 <%= div_for object.actor, :class => '' do %>
+  <%= javascript_tag do %>
+    <%= modal_for(object.actor.subject) %>
+  <% end %>
   <ul class="thumbnails span">
     <li class="span1">
       <a data-toggle="modal" href="#user-modal-<%= object.actor.slug %>" class="user-modal-button-<%= object.actor.slug %> container follower-tooltip" title="<%=truncate(object.actor.name, :length => 20)%>" rel="tooltip">
@@ -11,9 +14,6 @@
       </a>
     </li>
   </ul>
-<% end %>
-<%= javascript_tag do %>
- <%= modal_for(object.actor.subject) %>
 <% end %>
 <% end %>
 <div class="row space_center"></div>
@@ -22,6 +22,9 @@
 <% subject.following_actor_objects.includes(:actor).all.shuffle.first(4).each do |object|  %>
 
 <%= div_for object.actor, :class => '' do %>
+  <%= javascript_tag do %>
+    <%= modal_for(object.actor.subject) %>
+  <% end %>
   <ul class="thumbnails span">
     <li class="span1">
       <a data-toggle="modal" href="#user-modal-<%= object.actor.slug %>" class="user-modal-button-<%= object.actor.slug %> container follower-tooltip" title="<%=truncate(object.actor.name, :length => 20)%>" rel="tooltip">
@@ -29,11 +32,6 @@
       </a>
     </li>
   </ul>
-<% end %>
-<%= javascript_tag do %>
-  $(".user-modal-button-<%= object.actor.slug %>").on("click", function(){
-   <%= modal_for(object.actor.subject) %>
-  });
 <% end %>
 <% end %>
 

app/views/videos/_video_processed.html.erb
@@ -1,3 +1,6 @@
+<%= javascript_tag do %>
+  <%= modal_for(video) %>
+<% end %>
 <ul class="thumbnails">
   <li class="span3">
     <a data-toggle="modal" href="#video-modal-<%= video.id.to_s%>" class="video-modal-button-<%= video.id.to_s%>"><%= thumb_for(video, 75) %></a>
@@ -17,9 +20,4 @@
     </div>
   </li>
 </ul>
-
-<%= javascript_tag do %>
-  <%= modal_for(video) %>
-<% end %>
-
 