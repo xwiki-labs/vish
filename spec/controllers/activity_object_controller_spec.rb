43216c321bdde89a16e7deaa27dd220ef44f11e4
abenitoc <vermibeto@gmail.com>
2015-04-29 19:07:13 +0200

    Test(18) - Added test for avatars ao controller

--- spec/controllers/activity_object_controller_spec.rb
+++ spec/controllers/activity_object_controller_spec.rb
@@ -1,20 +1,22 @@
 require 'spec_helper'
 
 describe ActivityObjectController, controllers: true, debug: true do
-	#TODO
 	
 	context "show_avatar" do
 		it 'default_avatar?' do
 
 			@userAO = Factory(:user_vish)
-
-			get :show_avatar, :params[id] => @userAO.activity_object.id.to_i
+			
+			get :show_avatar, :params => { id: @userAO.activity_object.id }
 
 			assert_response :success
 		end
-		
-		it 'non_existing?'
-		it 'custom_avatar?'
 
+		it 'custom_avatar?' do
+			@picture = Factory(:user_vish)
+			@userAO = Factory(:user_vish, avatar: "http://lorempixel.com/400/200/sports/" )
+			get :show_avatar, :params => { id: @userAO.activity_object.id }
+			assert_response :success
+		end
 	end
 end
