aeebac8e42d15cac7be3f031a0959edbb21a552a
Berto_ABC <vermibeto@gmail.com>
2016-05-24 22:17:10 +0200

    Added aproximately to profile

--- app/controllers/dali_documents_controller.rb
+++ app/controllers/dali_documents_controller.rb
@@ -1,9 +1,10 @@
 class DaliDocumentsController < ApplicationController
 	  include SocialStream::Controllers::Objects
 
+    before_filter :merge_json_params
     before_filter :cors_preflight_check, :only => [:create]
-    after_filter :cors_set_access_control_headers, :only => [:create]
-    skip_load_and_authorize_resource :only => [ :create_document]
+    after_filter :cors_set_access_control_headers, :only => [:create, :merge_json_params]
+    skip_load_and_authorize_resource :only => [:create_document, :update_document]
     skip_before_filter  :verify_authenticity_token
 
     def new
@@ -15,9 +16,17 @@ class DaliDocumentsController < ApplicationController
     def create_document
 
       dd = DaliDocument.new
-      dd.json = params[:dali_document][:json].to_json
+      dd.json = params[:dali_document].to_json
       dd.owner_id = params[:user_data]
       dd.author_id = params[:user_data]
+      dd.save!
+
+      render json: { dali_id: dd.id}
+    end
+
+    def update_document
+      dd = DaliDocument.find(params[:id])
+      dd.json = params[:dali_document].to_json
       dd.save!
 
       render json: { dali_id: dd.id}
@@ -41,4 +50,19 @@ class DaliDocumentsController < ApplicationController
       end
     end
     
+ 
+private
+  
+  #If you dear programmer are asking ¡what is this method doing', just to mention, rails makes a terrible conversion from json to Ruby arrays
+  #and basically changes all empty arrays for nils (this is for security purposes, but seems like killing flies by cannonballs)
+  #So we need to fix this somehow because our json app does things like that for some reason. I Hope it helped
+
+  def merge_json_params
+    if request.format.json?
+      body = request.body.read
+      request.body.rewind
+      params.merge!(ActiveSupport::JSON.decode(body)) unless body == ""
+    end
+  end
+
 end
